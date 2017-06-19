ef3f4970bdc8e7bbede9aa11d7b87a97bb3b42c3
bacante <bacante@gmail.com>
2017-06-19 12:31:41 +0200

    W3C - tira líneas ... done

--- Prototypes/Engine/src/es/geoplanosocial/levels/world3/c/Level3C.java
+++ Prototypes/Engine/src/es/geoplanosocial/levels/world3/c/Level3C.java
@@ -3,8 +3,13 @@ package es.geoplanosocial.levels.world3.c;
 import es.geoplanosocial.levels.Level;
 import es.geoplanosocial.players.Player;
 import es.geoplanosocial.util.Color;
+import es.geoplanosocial.util.Constants;
 
 import java.util.ArrayList;
+
+import static processing.core.PApplet.constrain;
+import static processing.core.PApplet.dist;
+import static processing.core.PApplet.map;
 
 /**
  * World 3
@@ -15,9 +20,14 @@ public class Level3C extends Level {
 
     private static final String TITLE="Iron triangle";
     public static final int MAIN_COLOR= Color.W3_C_BG;
+    private static final float STROKEWEIGHT_LEVEL3B = 10;
+
+
+    private float[][] ancho;
 
     public Level3C() {
         super(TITLE, MAIN_COLOR);
+        setDrawPlayersFront(true);
     }
 
 
@@ -42,14 +52,35 @@ public class Level3C extends Level {
 
     @Override
     public void update() {
-        //TODO Update level elements
-
+        ancho = new float[this.players.size()][this.players.size()];
+        for (int i = 0; i < this.players.size(); i++)
+            for (int j = 0; j < this.players.size(); j++)
+                if (i != j) {
+                    float x1 = (float)players.get(i).getLocation().getX();
+                    float y1 = (float)players.get(i).getLocation().getY();
+                    float x2 = (float)players.get(j).getLocation().getX();
+                    float y2 = (float)players.get(j).getLocation().getY();
+                    float distancia = dist(x1, y1, x2, y2);
+                    ancho[i][j] = map(distancia, 0, Constants.LEVEL_WIDTH, 0, STROKEWEIGHT_LEVEL3B);
+                    ancho[i][j] = constrain(ancho[i][j], 0, STROKEWEIGHT_LEVEL3B);
+                }
     }
 
 
     @Override
     protected void drawLevel() {
-        //TODO Draw level elements
+        pg.beginDraw();
+        pg.stroke(Color.W2_WHITE_NODE);
+        for (int i = 0; i < this.players.size(); i++)
+            for (int j = 0; j < this.players.size(); j++)
+                if (i != j) {
+                    pg.strokeWeight(STROKEWEIGHT_LEVEL3B - ancho[i][j]);
+                    pg.line((float)players.get(i).getLocation().getX(),
+                            (float)players.get(i).getLocation().getY(),
+                            (float)players.get(j).getLocation().getX(),
+                            (float)players.get(j).getLocation().getY());
+                }
+        pg.endDraw();
     }
 
 
